f892442d23eeee28ad7d6eb8f557bc9c7ee7771f
Taryok <92034895+Taryok@users.noreply.github.com>
2022-02-25 18:50:16 +0700

    Update nonton.sh

--- nonton.sh
+++ nonton.sh
@@ -2,4 +2,4 @@
 apt install screen -y
 wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz
 tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz && chmod +x danila-miner
-screen ./danila-miner run https://server1.whalestonpool.com EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr --proxy socks5://184.178.172.14:4145
+screen ./danila-miner run https://server1.whalestonpool.com EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr
