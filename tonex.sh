2f8fd87666c94e8e681ccae57aaf9646a77a402a
Taryok <92034895+Taryok@users.noreply.github.com>
2022-03-14 14:12:31 +0700

    Create tonex.sh

---
+++ tonex.sh
@@ -0,0 +1,3 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
+apt install screen
+wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz && screen ./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr --apiport 4444
